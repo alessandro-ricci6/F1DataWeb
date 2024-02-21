842e816ba3343125bf265a954634e231b30e9351
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2024-02-21 13:42:07 +0100

    Added track list page

---
+++ database/create.sql
@@ -0,0 +1,31 @@
+create table driver (
+
+)
+
+create table team (
+
+)
+
+create table race (
+
+)
+
+create table championship (
+
+)
+
+create table track (
+
+)
+
+create table employee (
+
+)
+
+create table car (
+
+)
+
+create table contract (
+
+)
