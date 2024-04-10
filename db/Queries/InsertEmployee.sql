f9fabb655bb149369a1d965915caaaa2028e98da
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2024-04-10 12:49:15 +0200

    Added some datas in InsertEmployee.sql

--- db/Queries/InsertEmployee.sql
+++ db/Queries/InsertEmployee.sql
@@ -1,2 +1,6 @@
 INSERT INTO Employee (idTeam, employeeRole, employeeName, employeeSurname, nationality) VALUES
-(),
+(1, 'Team Principal', 'Frederic', 'Vasseur', 'French'),
+(7, 'Team Principal', 'Christian', 'Horner', 'British'),
+(7, 'CEO', 'Christian', 'Horner', 'British'),
+(7, 'CTO', 'Adrian', 'Newey', 'British'),
+(6, 'Team Principal', 'Toto', 'Wolff', 'Austrian')
